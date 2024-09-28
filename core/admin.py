9d4684b8de3847588dbd0be9d7681956358ae882
chintansheth2001 <chintansheth2001@gmail.com>
2024-09-28 13:45:07 +0000

    Added models in app

--- core/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from core.models import Restaurant, Rating, Sale
-
-# Register your models here.
-admin.site.register(Restaurant)
-admin.site.register(Rating)
-admin.site.register(Sale)
