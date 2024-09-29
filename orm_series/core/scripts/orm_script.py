62e1687ebf770d9bed1ee9cb1b28bac3fa66c568
chintansheth2001 <chintansheth2001@gmail.com>
2024-09-29 10:47:19 +0000

    add pprint to show proper sql query with save method

--- orm_series/core/scripts/orm_script.py
+++ orm_series/core/scripts/orm_script.py
@@ -1,11 +1,17 @@
-from core.models import Restaurant
+from django.contrib.auth.models import User
+from core.models import Restaurant, Rating
 from django.utils import timezone
 from django.db  import connection
+from pprint import pprint
 
 def run():
     
-    print(Restaurant.objects.count())
+    restaurant = Restaurant.objects.last()
+    restaurant.name = "Jashuben na Pizza"
+    restaurant.save()
+    # user = User.objects.first()
+    # rating = Rating.objects.create(user=user,restaurant=restaurant, rating=3 )
     
-    print(connection.queries) 
+    pprint(connection.queries) 
 
     
