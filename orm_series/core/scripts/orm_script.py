d8616ff9ea578b270ef36534f90abc7f32581e09
chintansheth2001 <chintansheth2001@gmail.com>
2024-09-29 10:55:55 +0000

    Query detail to master or child to parent

--- orm_series/core/scripts/orm_script.py
+++ orm_series/core/scripts/orm_script.py
@@ -5,13 +5,10 @@ from django.db  import connection
 from pprint import pprint
 
 def run():
-    
-    restaurant = Restaurant.objects.last()
-    restaurant.name = "Jashuben na Pizza"
-    restaurant.save()
-    # user = User.objects.first()
-    # rating = Rating.objects.create(user=user,restaurant=restaurant, rating=3 )
-    
+
+    rating = Rating.objects.first()
+    print(rating.restaurant)  
+   
     pprint(connection.queries) 
 
     
